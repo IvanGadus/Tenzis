e511fde7621871f50cb661a763e43d2c78e57b6e
Ivan <ivan.gadus01@gmail.com>
2022-09-14 19:35:03 +0200

    zmena farby if isHeld

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export default function Die(props) {
+    const style = {
+        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+    }
   return (
-    <button onClick={()=>props.holdDice(props.id)} className='dieBtn'>{props.value}</button>
+    <button style={style} onClick={()=>props.holdDice(props.id)} className='dieBtn'>{props.value}</button>
   )
 }
