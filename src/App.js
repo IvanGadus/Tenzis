4b5729d397132d1b50524a39a9ad9dcd9861351e
Ivan <ivan.gadus01@gmail.com>
2022-09-19 13:33:54 +0200

    oprava bugov pri resetovani hry

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ export default function App() {
   const [dice, setDice] = useState(allNewNumbers())  
   const [tenzis, setTenzis] = useState(false)
   const [attempts, setAttempts] = useState(0)
-  console.log(tenzis)
+  const [playTime, setPlayTime] = useState(0)
 
   useEffect(()=>{
     const value = dice[0].value
-    setAttempts(prevAtempt => prevAtempt+1)
     if(dice.every((die)=>die.value===value && die.isHeld===true)){
       setTenzis(true)
     } 
@@ -47,15 +46,23 @@ export default function App() {
   
 
   function rollDice(){
-    setDice((prevDice)=>{
-      return(
-        prevDice.map((die)=>{
-          return(
-            die.isHeld ? die : {...die, value:Math.floor(Math.random()*6)+1}
-          )
-        })
-      )
-    })
+    if(!tenzis){
+      setAttempts(prevAtempt => prevAtempt+1)
+      setDice((prevDice)=>{
+        return(
+          prevDice.map((die)=>{
+            return(
+              die.isHeld ? die : {...die, value:Math.floor(Math.random()*6)+1}
+            )
+          })
+        )
+      })
+    }else{
+      setDice(allNewNumbers())
+      setAttempts(0)
+      setTenzis(false)
+    }
+
   }
 
 
@@ -78,7 +85,7 @@ export default function App() {
           <div className="diceContainer">
             {generateDice}
           </div>
-          <button onClick={rollDice} className='rollBtn'>Hoď kockami</button>
+          <button onClick={rollDice} className='rollBtn'>{!tenzis ? "Hoď kockami" : "Reštartovať hru"}</button>
           <p>Tvoje pokusy: <strong>{attempts}</strong></p>
         </main>
     </div>
